7d1dceac40af7333f0a4fba3c1da7189fbf44ddf
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2016-08-05 15:49:36 +0100

    Added Google Analytics

--- api/views/welcome_message.php
+++ api/views/welcome_message.php
@@ -84,6 +84,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
 </div>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
+  ga('create', 'UA-82063434-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </body>
 </html>
